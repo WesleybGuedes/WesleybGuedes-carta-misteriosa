d9cec8930786d9c2280d38f0962b012f78d791cb
WesleybGuedes <wesleyguedes2013@gmail.com>
2024-08-20 00:11:35 -0300

    feat(script.js): use foreach and create new mode for classes

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
-const button = document.getElementById("criar-carta");
-const cartaGerada = document.getElementById("carta-gerada");
+const classes = {
+  estilos: ["newspaper", "magazine1", "magazine2"],
+  tamanhos: ["medium", "big", "reallybig"],
+  rotacoes: ["rotateleft", "rotateright"],
+  inclinacoes: ["skewleft", "skewright"],
+};
 
-const estilos = ["newspaper", "magazine1", "magazine2"];
-const tamanhos = ["medium", "big", "reallybig"];
-const rotacoes = ["rotateleft", "rotateright"];
-const inclinacoes = ["skewleft", "skewright"];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+function getRandomInt(min, max) {
+  const mini = Math.ceil(min);
+  const maxi = Math.floor(max);
+  return Math.floor(Math.random() * (maxi - mini + 1)) + mini;
 }
 
 function criarSpanComClassesAleatorias(texto) {
   const span = document.createElement("span");
   span.textContent = texto;
 
-  const estiloAleatorio = estilos[getRandomInt(estilos.length)];
-  const tamanhoAleatorio = tamanhos[getRandomInt(tamanhos.length)];
-  const rotacaoAleatoria = rotacoes[getRandomInt(rotacoes.length)];
-  const inclinacaoAleatoria = inclinacoes[getRandomInt(tamanhos.length)];
+  for (let i = 0; i < Object.values(classes).length; i += 1) {
+    const grupo = Object.values(classes)[i];
+    const indiceAleatorio = getRandomInt(0, grupo.length - 1);
+    span.classList.add(grupo[indiceAleatorio]);
+  }
 
-  span.classList.add(estiloAleatorio, tamanhoAleatorio, rotacaoAleatoria, inclinacaoAleatoria);
   return span;
 }
-
 function gerarCarta(texto) {
+  const cartaGerada = document.getElementById("carta-gerada");
   cartaGerada.innerHTML = "";
+
   const palavras = texto.split(" ");
+  // Substituindo for...of por forEach
   palavras.forEach((palavra) => {
     const span = criarSpanComClassesAleatorias(palavra);
     cartaGerada.appendChild(span);
   });
 }
 
+// Obtém os elementos do DOM
+const button = document.getElementById("criar-carta");
+const input = document.getElementById("carta-texto");
+
+// Adiciona um ouvinte de evento ao botão para gerar a carta
 button.addEventListener("click", () => {
-  const input = document.getElementById("carta-texto");
   const texto = input.value;
 
   if (texto.trim() === "") {
-    cartaGerada.innerText = "Por favor, digite o conteúdo da carta.";
+    alert("Por favor, digite o conteúdo da carta.");
   } else {
     gerarCarta(texto);
   }
