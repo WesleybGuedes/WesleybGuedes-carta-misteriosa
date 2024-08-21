a8b5ad2f763d024913048130dc5e4d1b47e9e370
WesleybGuedes <wesleyguedes2013@gmail.com>
2024-08-20 21:50:03 -0300

    feat(script.js): modify things

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const cartaGerada = document.getElementById("carta-gerada");
+const button = document.getElementById("criar-carta");
+const input = document.getElementById("carta-texto");
+
 const classes = {
   estilos: ["newspaper", "magazine1", "magazine2"],
   tamanhos: ["medium", "big", "reallybig"],
@@ -24,27 +28,20 @@ function criarSpanComClassesAleatorias(texto) {
   return span;
 }
 function gerarCarta(texto) {
-  const cartaGerada = document.getElementById("carta-gerada");
   cartaGerada.innerHTML = "";
 
   const palavras = texto.split(" ");
-  // Substituindo for...of por forEach
   palavras.forEach((palavra) => {
     const span = criarSpanComClassesAleatorias(palavra);
     cartaGerada.appendChild(span);
   });
 }
 
-// Obtém os elementos do DOM
-const button = document.getElementById("criar-carta");
-const input = document.getElementById("carta-texto");
-
-// Adiciona um ouvinte de evento ao botão para gerar a carta
 button.addEventListener("click", () => {
   const texto = input.value;
 
   if (texto.trim() === "") {
-    alert("Por favor, digite o conteúdo da carta.");
+    cartaGerada.textContent = "Por favor, digite o conteúdo da carta.";
   } else {
     gerarCarta(texto);
   }
